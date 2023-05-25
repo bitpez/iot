9cab197cb416f738c323a0a60beaf25780a1e51f
Pro <62343019+bitpez@users.noreply.github.com>
2023-05-24 23:38:26 -0500

    Initial repository

---
+++ Emulator/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.IO.Ports;
+
+SerialPort serialPort = new()
+{
+    BaudRate = 9600,
+    PortName = "COM3",
+};
+
+serialPort.Open();
+serialPort.WriteLine("");
+
+while (true) 
+{
+    string outPut = serialPort.ReadLine();
+    
+    if (outPut == "All is done.\r")
+    {
+        serialPort.WriteLine("");
+    }
+    else
+    {
+        Console.WriteLine(outPut);
+    }
+}
